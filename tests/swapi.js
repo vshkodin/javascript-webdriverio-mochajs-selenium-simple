f2f6021d2e67bf10765f972e7daa5bd846a766d9
vsshk <v.s.shkodin@gmail.com>
2021-03-27 21:55:58 -0700

    added extra steps

--- tests/swapi.js
+++ tests/swapi.js
@@ -1,6 +1,11 @@
 describe('My Login application', () => {
     it('should login with valid credentials', () => {
         browser.url('https://swapi.dev/');
+
+	$('a[href="/about"]').click();
+	$("a[href='/documentation']").click();
+	browser.saveScreenshot('screenshot.png');
+	//browser.click("a[href='/documentation']");
     });
 });
 
